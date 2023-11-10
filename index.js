f37b0268e1352b85b6af08b72b683de5201d6edd
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-10 01:48:33 -0700

    completed acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+
+  const dodger = getElementById("dodger");
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "Arrowleft") {
+        moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "Arrowright") {
+        moveDodgerRight();
+    }
+  });
